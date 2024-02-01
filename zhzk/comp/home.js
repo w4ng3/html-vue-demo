f8a05e1d1b5e6c08944746f01cb118193a98bcb1
w2gd <W2GDong@gmail.com>
2024-02-01 17:23:37 +0800

    fea:update

--- zhzk/comp/home.js
+++ zhzk/comp/home.js
@@ -1,41 +1,60 @@
 const Home = {
   setup() {
-    const { reactive, toRefs, ref, computed, onMounted } = Vue;
-    const { useRoute } = VueRouter
-    const route = useRoute()
+    const { ref, onMounted } = Vue;
+    // const { useRoute } = VueRouter
+    // const route = useRoute()
 
     let wxAppid = "wx31cf25d1328da3ca";
-    let redirect_uri = 'https://box.viperyibaba.com/zhzk/index.html';
+    // let redirect_uri = 'https://box.viperyibaba.com/zhzk/index.html#/';
+    let redirect_uri = encodeURIComponent('https://box.viperyibaba.com/zhzk/index.html#/')
 
-    const userOpenid = ref('openidxxx')
+    const userOpenid = ref('code---')
     /** 
      * @description 状态
      * 'success' 开门成功，'fail' 开门失败，'register': 需要绑定手机号，'loading':等待中
      */
     const status = ref('loading')
 
+    // 获取当前url的params参数
+    function getParams() {
+      let url = window.location.href;
+      let params = {};
+      if (url.indexOf("?") != -1) {
+        let arr = url.split("?")[1].split("&");
+        for (let i = 0; i < arr.length; i++) {
+          let key = arr[i].split("=")[0];
+          let value = arr[i].split("=")[1];
+          params[key] = value;
+        }
+      }
+      return params;
+    }
+
     onMounted(() => {
-      let params = route.query
-      console.log('param', params.code);
+      // let params = route.query
+      let params = getParams();
+      console.log('code:', params.code);
       // 判断是否有code参数
       if (params.code) {
         // 有code参数，说明是从微信授权页面跳转过来的
         let code = params.code;
+        // 测试，待删～
+        userOpenid.value = code
         // /** 开门 */
         apipost(`/zhzk/getCode?code=${code}`, {}).then(res => {
-          // 1.如果微信用户未绑定，显示表单
+          // 1. 开门失败
           if (res.code == 0) {
-            let data = res.data
-            userOpenid.value = data.openid
-            status.value = 'register'
+            status.value = 'fail'
           }
           // 2. 开门成功
           else if (res.code == 1) {
             status.value = 'success'
           }
-          // 3. 开门失败
+          // 3. 需要绑定
           else {
-            status.value = 'fail'
+            let data = res.data
+            // userOpenid.value = data.openid
+            status.value = 'register'
           }
         }).catch(err => { alert('网络错误') })
       } else {
@@ -44,7 +63,6 @@ const Home = {
           `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wxAppid}&redirect_uri=${redirect_uri}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
       }
     })
-
 
     /** 
      * 手机号表单
@@ -55,7 +73,15 @@ const Home = {
     });
 
     const formRules = {
-      mobile: [{ required: true, message: '请输入手机号', trigger: 'blur' }],
+      mobile: [
+        { required: true, message: '请输入手机号', trigger: 'blur' },
+        // 正则，手机号
+        {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '请输入正确的手机号',
+          trigger: 'blur'
+        }
+      ],
       verificationCode: [
         { required: true, message: '请输入验证码', trigger: 'blur' }
       ]
@@ -65,13 +91,18 @@ const Home = {
 
     async function getVerificationCode() {
       try {
-        // 发送获取验证码的请求，假设这里使用了异步函数
-        // 示例：调用一个获取验证码的API
-        // 这里应该包括一些逻辑来确保在倒计时期间防止重复点击获取验证码
+        // 正则校验手机号，失败则不发送请求
+        const mobile = formData.value.mobile;
+        if (!/^1[3456789]\d{9}$/.test(mobile)) {
+          return;
+        }
+        // 发送获取验证码的请求
+        await apipost(`/zhzk/getVertifyCode`, { phone: mobile })
         // 如果成功发送请求，则开始倒计时
         await startCountDown();
       } catch (error) {
         console.error('获取验证码失败', error);
+        alert('获取验证码失败')
       }
     }
 
@@ -88,9 +119,18 @@ const Home = {
     }
 
     async function submitForm() {
+      // 如果表单校验失败，则不提交
+      if (!formData.value.mobile || !formData.value.verificationCode) {
+        return;
+      }
       try {
         // 在这里执行表单提交的逻辑，可以调用API等
         console.log('表单提交成功', formData.value);
+        await apipost(`/zhzk/register`, {
+          phone: mobile,
+          code: formData.value.verificationCode,
+          userOpenid: userOpenid.value
+        })
       } catch (error) {
         console.error('表单提交失败', error);
       }
@@ -105,7 +145,8 @@ const Home = {
       getVerificationCode,
       submitForm,
       status,
-      agreeTerms
+      agreeTerms,
+      userOpenid
     };
   },
   template: '#home-template',
